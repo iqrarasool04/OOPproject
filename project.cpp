7e363bcf00fc1733db260b0d57806c8a0ec2873b
abs2002a <129922695+abs2002a@users.noreply.github.com>
2023-07-30 16:16:55 +0500

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -1,57 +1,69 @@
 #include <iostream>
 #include <string>
 #include <map>
+#include <portaudio.h>
+
+
 using namespace std;
 
-class Piano{
-    private:
+class Piano {
+private:
     string note;
     double frequency;
 
-    protected:
-    map<string, double> notes;
+public:
+    struct NoteFrequency {
+        std::string note;
+        double frequency;
+    };
 
-    notes["C"] = 261.63;
-    notes["C#"] = 277.18;
-    notes["D"] = 293.66;
-    notes["D#"] = 311.13;
-    notes["E"] = 329.63;
-    notes["F"] = 349.23;
-    notes["F#"] = 369.99;
-    notes["G"] = 392.00;
-    notes["G#"] = 415.30;
-    notes["A"] = 440.00;
-    notes["A#"] = 466.16;
-    notes["B"] = 493.88;
+    NoteFrequency notes[12] = {
+        {"C", 261.63},
+        {"C#", 277.18},
+        {"D", 293.66},
+        {"D#", 311.13},
+        {"E", 329.63},
+        {"F", 349.23},
+        {"F#", 369.99},
+        {"G", 392.00},
+        {"G#", 415.30},
+        {"A", 440.00},
+        {"A#", 466.16},
+        {"B", 493.88}
+    };
 
-    public:
-    Piano(){
+public:
+
+
+    Piano() {
         note = "";
         frequency = 0;
     }
-    Piano(string n, double freq){
+    Piano(string n, double freq) {
         set_note(n);
         set_freq(freq);
     }
-    void set_note(string n){
+    void set_note(string n) {
         note = n;
     }
-    void set_freq(double f){
+    void set_freq(double f) {
         frequency = f;
     }
-    string get_note(){
+    string get_note() {
         return note;
     }
-    double get_freq(){
+    double get_freq() {
         return frequency;
-    }  
+    }
 };
-class Sound: public Piano{
-    private:
 
-    public:
-    
+
+class Sound : public Piano {
+private:
+
+public:
+
 };
-class Keyboard: public Piano{
+class Keyboard : public Piano {
 
-;
+};
